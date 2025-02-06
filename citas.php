5b85c4e3bda400720dc67ce2768e474cdca1cf55
Antia <antiagarrido@gmail.com>
2025-02-06 10:40:59 +0100

    consulta

--- citas.php
+++ citas.php
@@ -10,62 +10,77 @@ $conexion = new mysqli($servername, $username, $password, $dbname);
 
 
 $hora_incio=8;
-$hora_fin=10;
+$hora_fin=9;
+
+$dias_agenda=7;
 
 
 $hoy = date("d-m-Y");
 
-$fechafinstring  = strtotime($hoy."+ 7 days");
 
-$fecha_fin = date("d-m-Y", $fechafinstring);
+$citas_ocupadas= array();
 
-?>
- <h2>Citas Disponibles</h2>
- <table class="table table-hover">
-       
-    <thead> 
- 
-        <tr>
-            <th>Fecha</th>
-            <th>Hora</th>
-            <th>Estado</th>
-        </tr>
 
-    </thead>
-    <tbody>
+$sql_citas = "SELECT fecha_cita, hora_cita FROM citas ";
 
-<?php
 
-for($dia=$hoy; strtotime($dia)<= strtotime($fecha_fin); $i = date("d-m-Y", strtotime($dia."+ 1 days"))){
+$citas= $conexion->query($sql_citas);
 
-        for($hora=$hora_incio; $hora<$hora_fin; $hora++ ){
-            if($hora <10){
-                $hora = "0$hora";
-            }
-            for ($minuto = 0; $minuto < 60; $minuto += 5) {
+foreach ($citas as $cita){
 
-                if($minuto<10){
+    $fecha= $cita["fecha_cita"];
+    $hora= $cita["hora_cita"];
 
-                    $minuto= "0$minutos"
-                }
+    $fecha_formateada = date("d-m-Y", strtotime($fecha));
 
-            
+    array_push($citas_ocupadas, array($fecha_formateada, $hora));
 
-                
-
-            }
-        }
+   
 }
 
 
+
 ?>
+ <h2 class="text text-center">Citas</h2>
 
+
+ <div class="d-flex flex-wrap justify-content-center">
+<?php
+
+    for($dia=$hoy; strtotime($dia)<= strtotime($hoy."+ $dias_agenda days"); $dia = date("d-m-Y", strtotime($dia."+ 1 days"))){
+
+
+            echo "<table class='table table-hover table-sm me-3 mb-3 text-center' style='width: 200px;'>
+                    <thead> 
+                        <th> $dia</th> 
+                    </thead>
+                    
+                    <tbody>";            
     
+            for($hora=$hora_incio; $hora<$hora_fin; $hora++ ){
+                if($hora <10){
+                    $hora = "0$hora";
+                }
 
 
-    </tbody>
+                for ($minutos = 0; $minutos < 60; $minutos += 5) {
+
+                    if($minutos<10){
+
+                        $minutos= "0$minutos";
+                    }
 
 
+                    echo "<tr><td>$hora:$minutos <i class='bi bi-circle-fill ms-2'></i></i></td></tr>";
 
+                    
 
-</table>
+                }
+            }
+
+            echo "  </table>";
+    }
+
+    ?>
+</div>
+        
